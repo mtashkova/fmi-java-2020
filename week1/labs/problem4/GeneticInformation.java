37c39a2867c01fe2b3eb1e3a9999d50d4a8b1dc0
mtashkova <67580071+mtashkova@users.noreply.github.com>
2020-10-24 17:07:03 +0300

    Add files via upload

--- week1/labs/problem4/GeneticInformation.java
+++ week1/labs/problem4/GeneticInformation.java
@@ -1,30 +1,51 @@
 package problem4;
 
 public class GeneticInformation {
+    public int convertToFour(int number) {
+        int digit1,
+                digit2,
+                digit3,
+                digit4,
+                convertedNumber;
+        digit1 = number % 4;
+        number /= 4;
+        digit2 = number % 4;
+        number /= 4;
+        digit3 = number % 4;
+        number /= 4;
+        digit4 = number % 4;
+        convertedNumber = digit1 + digit2 * 10 + digit3 * 100 + digit4 * 1000;
+        return convertedNumber;
+    }
+
     public void convertDigits(int number) {
         //declaration
         int[] digit = new int[4];
         String str;
         //initialization
         str = "";
-        digit[3] = number%10;
-        number/=10;
-        digit[2] = number%10;
-        number/=10;
-        digit[1] = number%10;
-        number/=10;
-        digit[0] = number%10;
-        number/=10;
+        digit[3] = number % 10;
+        number /= 10;
+        digit[2] = number % 10;
+        number /= 10;
+        digit[1] = number % 10;
+        number /= 10;
+        digit[0] = number % 10;
+        number /= 10;
         // processing
-        for(int i=0; i<4 ;i++) {
-            switch(digit[i]) {
-                case 0 : str = str.concat("A");
+        for (int i = 0; i < 4; i++) {
+            switch (digit[i]) {
+                case 0:
+                    str = str.concat("A");
                     break;
-                case 1 : str = str.concat("C");
+                case 1:
+                    str = str.concat("C");
                     break;
-                case 2 : str = str.concat("G");
+                case 2:
+                    str = str.concat("G");
                     break;
-                case 3 : str = str.concat("T");
+                case 3:
+                    str = str.concat("T");
                     break;
             }
         }
--- week1/labs/problem4/GeneticinformationTest.java
+++ week1/labs/problem4/GeneticinformationTest.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class GeneticinformationTest {
     public static void main(String[] args) {
         Scanner kdb = new Scanner(System.in);
-        int number;
+        int number,
+            convertedNumber;
         GeneticInformation gi = new GeneticInformation();
-        System.out.print("Enter a 4-digit number: ");
+        System.out.print("Enter a number: ");
         number = kdb.nextInt();
-        gi.convertDigits(number);
+        convertedNumber = gi.convertToFour(number);
+        gi.convertDigits(convertedNumber);
     }
 }
